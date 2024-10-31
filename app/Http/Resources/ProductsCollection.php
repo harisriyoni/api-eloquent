2b558196e3ffe1a74d83c2a214d0f823bf3b744d
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-31 14:29:47 +0700

    tamattt

--- app/Http/Resources/ProductsCollection.php
+++ app/Http/Resources/ProductsCollection.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
@@ -18,5 +19,9 @@ class ProductsCollection extends ResourceCollection
         return [
             "data" => ProductResource::collection($this->collection),
         ];
+    }
+    public function withResponse(Request $request, JsonResponse $response)
+    {
+        $response->header("X-Powered-By", "Haris Riyoni");
     }
 }
