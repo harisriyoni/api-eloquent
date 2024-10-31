7276d3e5826c969e6a9dee3ebdbc9d48d1b2ec08
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-31 10:55:38 +0700

    category test

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -28,21 +28,50 @@ class CategoryTest extends TestCase
     {
         $this->seed([CategorySeeder::class]);
         $category = Category::all();
+
         $this->get("/api/categories")
             ->assertStatus(200)
             ->assertJson([
                 'data' => [
-                    'id' => $category[0]->id,
-                    'name' => $category[0]->name,
-                    'created_at' => $category[0]->created_at->toJSON(),
-                    'updated_at' => $category[0]->updated_at->toJSON(),
+                    [
+                        'id' => $category[0]->id,
+                        'name' => $category[0]->name,
+                        'created_at' => $category[0]->created_at->toJSON(),
+                        'updated_at' => $category[0]->updated_at->toJSON(),
+                    ],
+                    [
+                        'id' => $category[1]->id,
+                        'name' => $category[1]->name,
+                        'created_at' => $category[1]->created_at->toJSON(),
+                        'updated_at' => $category[1]->updated_at->toJSON(),
+                    ],
                 ],
-                [
-                    'id' => $category[1]->id,
-                    'name' => $category[1]->name,
-                    'created_at' => $category[1]->created_at->toJSON(),
-                    'updated_at' => $category[1]->updated_at->toJSON(),
-                ]
             ]);
     }
+    public function testCategoryResourceCollectionCustom()
+    {
+        $this->seed([CategorySeeder::class]);
+        $category = Category::all();
+
+        $this->get("/api/categories-custom")
+            ->assertStatus(200)     
+            ->assertJson([
+                'total' => 2,
+                'data' => [
+                    [
+                        'id' => $category[0]->id,
+                        'name' => $category[0]->name,
+                        'created_at' => $category[0]->created_at->toJSON(),
+                        'updated_at' => $category[0]->updated_at->toJSON(),
+                    ],
+                    [
+                        'id' => $category[1]->id,
+                        'name' => $category[1]->name,
+                        'created_at' => $category[1]->created_at->toJSON(),
+                        'updated_at' => $category[1]->updated_at->toJSON(),
+                    ],
+                ],
+            ]);
+    }
+
 }
